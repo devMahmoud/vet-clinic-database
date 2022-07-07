fe22f5a954d08394b704dfc09bcfc869d74ebbb4
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-07-07 19:20:42 +0200

    Add queries

--- queries.sql
+++ queries.sql
@@ -15,3 +15,19 @@ SELECT * from animals WHERE neutered = true;
 SELECT * from animals WHERE name NOT IN ('Gabumon');
 
 SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+SELECT COUNT(*) FROM animals;
+
+SELECT COUNT(escape_attempts) FROM animals WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT name, escape_attempts FROM animals WHERE escape_attempts = (SELECT MAX(escape_attempts) FROM animals);
+
+SELECT name, neutered FROM animals WHERE neutered = true;
+
+SELECT name, neutered FROM animals WHERE neutered = false;
+
+SELECT species, MAX(weight_kg), MIN(weight_kg) FROM animals GROUP BY species;
+
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN 'Jan-01-1990' AND 'Dec-31-2000' GROUP BY species;
