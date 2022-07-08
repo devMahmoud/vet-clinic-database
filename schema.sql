7a90f6c3e4542f6070f8d1a56b4029d836eefa54
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-07-08 23:34:11 +0200

    Add species and owners tables

--- schema.sql
+++ schema.sql
@@ -10,3 +10,27 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD species varchar(50);
+
+ALTER TABLE animals
+  DROP COLUMN species
+;
+
+CREATE TABLE owners (
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
+    full_name varchar(255),
+    age integer,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE species (
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
+    name varchar(255),
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE animals
+  ADD COLUMN species_id integer,
+  ADD COLUMN owner_id integer,
+  ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species(id),
+  ADD CONSTRAINT fk_owners FOREIGN KEY (owner_id) REFERENCES owners(id)
+;
